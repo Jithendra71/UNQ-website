ed7a621c971c6a75588da98fc134019f87d6aea7
Jithendra71 <jithendra4567890@gmail.com>
2021-11-19 19:09:42 +0530

    added primary backend

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+var formidable = require("express-formidable");
+app.use(formidable());
+
+var mongodb = require("mongodb");
+var mongoClient = mongodb.MongoClient;
+
+var http = require("http").createServer(app);
+
+app.use("/public", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+var mainURL = "http://localhost:3000";
+
+
+
+http.listen(3000, function () {
+
+	console.log("Server started at " + mainURL);
+	mongoClient.connect("mongodb://localhost:27017", function (error, client) {
+		var database = client.db("my_social_network");
+		console.log("Database connected. " + database);
+
+	});
+});
