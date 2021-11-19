11546b0dad0468aa5ac70c28ba57597804ec6e98
Jithendra71 <jithendra4567890@gmail.com>
2021-11-19 23:52:34 +0530

    added bcrypt and socket.io

--- server.js
+++ server.js
@@ -6,15 +6,28 @@ app.use(formidable());
 
 var mongodb = require("mongodb");
 var mongoClient = mongodb.MongoClient;
+var ObjectId = mongodb.ObjectId;
 
 var http = require("http").createServer(app);
+var bcrypt = require("bcrypt");
+var fileSystem = require("fs");
+
+var jwt = require("jsonwebtoken");
+var accessTokenSecret = "myAccessTokenSecret1234567890";
 
 app.use("/public", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+var socketIO = require("socket.io")(http);
+var socketID = "";
+var users = [];
 
 var mainURL = "http://localhost:3000";
 
+socketIO.on("connection", function (socket) {
+	console.log("User connected", socket.id);
+	socketID = socket.id;
+});
 
 http.listen(3000, function () {
 
