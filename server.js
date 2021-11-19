8162a87110773c5282afe387262f551d32f84a3d
Jithendra71 <jithendra4567890@gmail.com>
2021-11-19 21:57:34 +0530

    added serverside for login and signup

--- server.js
+++ server.js
@@ -27,8 +27,99 @@ http.listen(3000, function () {
 			result.render("signup");
 		});
 
+		app.post("/signup", function (request, result) {
+			var name = request.fields.name;
+			var username = request.fields.username;
+			var email = request.fields.email;
+			var password = request.fields.password;
+			var gender = request.fields.gender;
+			var reset_token = "";
+
+			database.collection("users").findOne({
+				$or: [{
+					"email": email
+				}, {
+					"username": username
+				}]
+			}, function (error, user) {
+				if (user == null) {
+					bcrypt.hash(password, 10, function (error, hash) {
+						database.collection("users").insertOne({
+							"name": name,
+							"username": username,
+							"email": email,
+							"password": hash,
+							"gender": gender,
+							"reset_token": reset_token,
+							"profileImage": "",
+							"coverPhoto": "",
+							"dob": "",
+							"city": "",
+							"country": "",
+							"aboutMe": "",
+							"friends": [],
+							"pages": [],
+							"notifications": [],
+							"groups": [],
+							"posts": []
+						}, function (error, data) {
+							result.json({
+								"status": "success",
+								"message": "Signed up successfully. You can login now."
+							});
+						});
+					});
+				} else {
+					result.json({
+						"status": "error",
+						"message": "Email or username already exist."
+					});
+				}
+			});
+		});
+
 		app.get("/login", function (request, result) {
 			result.render("login");
+		});
+
+		app.post("/login", function (request, result) {
+			var email = request.fields.email;
+			var password = request.fields.password;
+			database.collection("users").findOne({
+				"email": email
+			}, function (error, user) {
+				if (user == null) {
+					result.json({
+						"status": "error",
+						"message": "Email does not exist"
+					});
+				} else {
+					bcrypt.compare(password, user.password, function (error, isVerify) {
+						if (isVerify) {
+							var accessToken = jwt.sign({ email: email }, accessTokenSecret);
+							database.collection("users").findOneAndUpdate({
+								"email": email
+							}, {
+								$set: {
+									"accessToken": accessToken
+								}
+							}, function (error, data) {
+								result.json({
+									"status": "success",
+									"message": "Login successfully",
+									"accessToken": accessToken,
+									"profileImage": user.profileImage
+								});
+							});
+						} else {
+							result.json({
+								"status": "error",
+								"message": "Password is not correct"
+							});
+						}
+					});
+				}
+			});
 		});
 
 	});
